be49f814ec98f15fc0c2a2be77a471a21d9251c9
garzuzo <garzuxo@gmail.com>
2024-05-05 12:39:50 -0500

    feat(*): enable cors for pedals ui

--- Program.cs
+++ Program.cs
@@ -5,8 +5,16 @@ using pedals_api.Services.PedalService;
 using pedals_api.Services.ProductService;
 
 var builder = WebApplication.CreateBuilder(args);
+var allowSpecificOrigins = "pedals-ui";
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: allowSpecificOrigins, policy =>
+    {
+        policy.WithOrigins("http://localhost:5251");
+    });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -26,6 +34,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(allowSpecificOrigins);
 
 app.UseHttpsRedirection();
 
